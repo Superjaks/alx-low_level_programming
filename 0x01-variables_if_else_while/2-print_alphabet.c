3b3dbfac46f9d488f8bfb84c17aaf593bfad959b
Superjaks <adeeyo69@gmail.com>
2022-07-01 07:50:57 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
  * main - Entry point
  *
@@ -12,7 +10,7 @@ int main(void)
 
 	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
-		printf("\n");
+		putchar('\n');
 
 	return (0);
 }
