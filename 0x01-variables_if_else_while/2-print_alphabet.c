970854a0f2c84abdec15a08cd1a75042074e9883
Superjaks <adeeyo69@gmail.com>
2022-07-01 03:26:46 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * maion - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+		printf("\n");
+
+	return (0);
+}
