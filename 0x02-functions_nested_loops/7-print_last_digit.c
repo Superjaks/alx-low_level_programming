03da81d35d0982c487ca312e46d312ae8aa90ce3
Superjaks <adeeyo69@gmail.com>
2022-07-05 15:52:45 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+/*
+ * Fil;e: 7-print_last_digit.c
+ * Auth: Superjaks
+ */
+#include "main.h"
+/**
+ * _abs - a function that prints the last digit of a number
+ * @ab: integar input
+ * Return: absolute value of ab
+ */
+int _abs(int ab)
+{
+	return (ab * ((ab > 0) - (ab < 0)));
+}
