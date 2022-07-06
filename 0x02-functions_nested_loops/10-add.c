fac524a0167775b8fe78d0ae73af731b121bea59
Superjaks <adeeyo69@gmail.com>
2022-07-05 17:28:26 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+/*
+ * File: 10-add.c
+ * Auth: Superjaks
+ */
+#include "main.h"
+/**
+ * add - a function that adds two integers and returns the result
+ * @one: first number input
+ * @two: second number input
+ * Return: one + two
+ */
+int add(int one, int two)
+{
+	return (one + two);
+}
