bf2a6012fa3cf1f82cf8a29243b518eee2ed66fd
Superjaks <adeeyo69@gmail.com>
2022-07-07 05:38:37 -0700

    0-main.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,16 @@
+/*
+ * File: 100-times_table.c
+ * Auth: Superjaks
+ */
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_times_table - a function that prints the n times table
+ * @n: input
+ * Return: Always 0
+ */
+void print_times_table(int n)
+{
+
+	return (0);
+}
---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef HEADER_H
+#define HEADER_H
+/*
+ * File: main.h
+ * Auth: Superjaks
+ * Desc: haeder file for debugging
+ */
+void positive_or_negative(int i);
+#endif
