32c0929f28b588eac8434e7516958009f7c58b2e
Superjaks <adeeyo69@gmail.com>
2022-07-07 02:08:38 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+/*
+ * File: 2-mul.c
+ * Auth: Superjaks
+ */
+#include "main.h"
+/**
+ * mul - a function that multiplies two integers
+ * @a: first input
+ * @b: second input
+ * Return: multiply result of the two integars
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
