6ddb12540d08e4ecaf5050fa956f00807b012c01
Superjaks <adeeyo69@gmail.com>
2022-07-08 00:03:33 -0700

    2-largest_number.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,5 +4,11 @@
  * File: main.h
  * Auth: Superjaks
  */
+#include <stdio.h>
+
 void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int converty_day(int month, int day);
+
 #endif
