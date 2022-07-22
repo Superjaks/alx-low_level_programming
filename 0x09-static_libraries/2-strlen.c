7392a0660f45b519f228f0a7d2d964a537c5637e
Superjaks <adeeyo69@gmail.com>
2022-07-22 14:49:06 -0700

    libmy.a

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,22 @@
+/*
+ * File: 2-strlen.c
+ * Auth: Superjaks
+ */
+#include "main.h"
+/**
+ * _strlen - a function that returns the length of a string.
+ * @s: input char
+ * Return: return length of a string
+ */
+int _strlen(char *s)
+{
+	int i = 1, sum = 0;
+	char pl = s[0];
+
+	while (pl != '\0')
+	{
+		sum++;
+		pl = s[i++];
+	}
+	return (sum);
+}
---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - a function that copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+
+	while (i >= 0)
+	{
+		dest[i] = src[i];
+
+		if (src[i] == '\0')
+			return (dest);
+		i++;
+	}
+	return (dest);
+}
