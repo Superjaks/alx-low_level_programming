06226b640f825693ed9e9313f889a16ebdff2da2
Superjaks <adeeyo69@gmail.com>
2022-08-16 07:15:56 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,19 +4,16 @@ nclude "lists.h"
  * @h: head
  * Return: 0
  */
+
 size_t print_listint(const listint_t *h)
 {
-	int count = 0;
+	size_t nodes = 0;
 
-	if (h == NULL)
-		return (0);
-
-	while (h != NULL)
+	while (h)
 	{
+		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
-		count++;
 	}
-
-	return (count);
+	return (nodes);
 }
