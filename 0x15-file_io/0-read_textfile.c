b743fa8cdd050da14c4b38f6001803916dceeda0
Superjaks <adeeyo69@gmail.com>
2022-08-22 11:11:50 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,34 +6,30 @@
  * @letters: size
  * Return: 0
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+size_t read_textfile(const char *filename, size_t letters)
 {
-	int file, rd, wr;
+	int fd;
+	ssize_t nrd, nwr;
 	char *buf;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 
-	file = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 
-	if (file == -1)
+	if (fd == -1)
 		return (0);
 
-	buf = malloc(sizeof(char) * letters + 1);
-	if (buf == NULL)
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
 		return (0);
 
-	rd = read(file, buf, letters);
-	if (rd == -1)
-		return (0);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-	buf[letters] = '\0';
+	close(fd);
 
-	wr = write(1, buf, rd);
-	if (wr == -1)
-		return (0);
-
-	close(file);
 	free(buf);
-	return (wr);
+
+	return (nwr);
 }
